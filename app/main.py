0237a53557073ee7714b4cd5448e0e077ff459c6
shahiduljahid <shahiduljahid3@gmail.com>
2024-07-23 01:35:38 +0800

    asss

--- app/main.py
+++ app/main.py
@@ -6,11 +6,11 @@ from blog.routers import blog, user, authentication
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+# models.Base.metadata.create_all(bind=engine)
 
-app.include_router(blog.router)
-app.include_router(user.router)
-app.include_router(authentication.router)
+# app.include_router(blog.router)
+# app.include_router(user.router)
+# app.include_router(authentication.router)
 
 
 @app.get("/")
