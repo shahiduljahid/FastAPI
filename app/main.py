27b5a3548f64f17dc5333e7eae3d903533f0f7b2
shahiduljahid <shahiduljahid3@gmail.com>
2024-07-23 02:51:06 +0800

    xxx

--- app/main.py
+++ app/main.py
@@ -6,13 +6,9 @@ from blog.routers import blog, user, authentication
 
 app = FastAPI()
 
+
 models.Base.metadata.create_all(bind=engine)
 
 app.include_router(blog.router)
 app.include_router(user.router)
 app.include_router(authentication.router)
-
-
-@app.get("/")
-def hello_world():
-    return "Hello,World"
